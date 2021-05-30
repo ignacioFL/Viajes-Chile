24cbe79833955f903c149ba88f884488a22f7c26
Ignacio <ifernanlav@gmail.com>
2021-05-30 19:23:48 -0400

    se han modificado los archivos

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$("a").click(function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        var gato = this.hash;
+
+        $("html,body").animate({
+            scrollTop: $(gato).offset().scrollTop
+        }, 800, function () {
+            window.location.hash = gato;
+        });
+    }
+});
+
